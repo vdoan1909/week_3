a64847a20f18e22fc6e6a24fe0fa987f34c456d7
vdoann <openaivdoan@gmail.com>
2024-09-20 17:09:45 +0700

    Add trigger after update table loans

--- task_week_3.sql
+++ task_week_3.sql
@@ -3,11 +3,20 @@
 -- START TRANSACTION;
 
 -- INSERT INTO loans (book_id, user_id, loan_date, return_date, status)
--- VALUES ('3', '3', '2024-06-03', NULL, 'Borrowed');
+-- VALUES ('1', '1', '2024-03-03', NULL, 'Borrowed');
 
 -- COMMIT;
 
 -- ROLLBACK;
+
+START TRANSACTION;
+
+UPDATE loans SET status = 'Returned', return_date = NOW()
+WHERE id = 12;
+
+COMMIT;
+
+ROLLBACK;
 
 
 -- TRIGGER: after_insert_loans
@@ -22,7 +31,20 @@
 
 -- END;
 
+-- TRIGGER after_update_loans
+
+-- CREATE TRIGGER after_update_loans
+-- AFTER UPDATE ON loans
+-- FOR EACH ROW
+
+-- BEGIN 
+
+-- UPDATE books SET books.status = 'Available' WHERE books.id = NEW.book_id AND NEW.status = 'Returned';
+
+-- END;
+
 -- DROP TRIGGER IF EXISTS after_insert_loans
+-- DROP TRIGGER IF EXISTS after_update_loans
 
 -- VIEW
 
